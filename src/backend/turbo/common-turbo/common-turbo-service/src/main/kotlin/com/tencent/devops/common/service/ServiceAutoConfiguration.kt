6943388b1e34ba560c5dfcc0e2a78f87c5402cbe
Yuanruitao <1075097840@qq.com>
2023-03-13 11:28:55 +0800

    feat: Turbo 支持CoreDNS与Consul #8479

--- src/backend/turbo/common-turbo/common-turbo-service/src/main/kotlin/com/tencent/devops/common/service/ServiceAutoConfiguration.kt
+++ src/backend/turbo/common-turbo/common-turbo-service/src/main/kotlin/com/tencent/devops/common/service/ServiceAutoConfiguration.kt
@@ -3,10 +3,8 @@ package com.tencent.devops.common.service
 import com.tencent.devops.common.service.prometheus.BkTimedAspect
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
-import org.springframework.boot.autoconfigure.AutoConfigureBefore
 import org.springframework.boot.autoconfigure.AutoConfigureOrder
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.consul.ConsulAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
@@ -16,7 +14,6 @@ import org.springframework.core.env.Environment
 @Configuration
 @PropertySource("classpath:/common-service.properties")
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
-@AutoConfigureBefore(ConsulAutoConfiguration::class)
 @EnableDiscoveryClient
 class ServiceAutoConfiguration {
 
