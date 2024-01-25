b29d9a28dc1eac3bda8ce1f9588812c9721b84f1
RJ <eazence@163.com>
2024-01-25 11:30:20 +0800

    feat: Provide an interface for exporting Turbo compilation to accelerate machine resource usage #164

--- src/backend/turbo/api-turbo/src/main/kotlin/com/tencent/devops/turbo/api/IApigwMachineResourcesController.kt
+++ src/backend/turbo/api-turbo/src/main/kotlin/com/tencent/devops/turbo/api/IApigwMachineResourcesController.kt
@@ -6,7 +6,6 @@ import com.tencent.devops.turbo.vo.apiwg.MachineResourcesStatVO
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
-import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @Api(tags = ["OPENAPI_SERVER_RESOURCES"], description = "服务器资源查询接口")
 @RequestMapping("/open/machine/resources")
-@FeignClient(name = "turbo", contextId = "IApigwMachineResourcesController")
 interface IApigwMachineResourcesController {
 
     @ApiOperation("获取使用服务器资源统计")
