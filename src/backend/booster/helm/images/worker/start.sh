0faaeaa856168299e8a9cb9e95d59279bc659bc6
yanafu <yanafu@tencent.com>
2024-07-04 17:38:46 +0800

    feat: worker start.sh优化 #247

--- src/backend/booster/helm/images/worker/start.sh
+++ src/backend/booster/helm/images/worker/start.sh
@@ -42,8 +42,8 @@ fi
 echo "BK_DIST_MAX_PROCESS_4_WORKER=${BK_DIST_MAX_PROCESS_4_WORKER}"
 echo "BK_DIST_WHITE_IP=${BK_DIST_WHITE_IP}"
 
-if [ -f "${HOME}/.bashrc" ]; then
-echo "get bashrc file from ${HOME}"
+if [ -f ~/.bashrc ]; then
+echo "source bashrc file"
 source ~/.bashrc
 fi
 
