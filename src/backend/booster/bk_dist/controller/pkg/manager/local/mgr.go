a4abc10187901b1943354431a2bbeb962661d841
yanafu <yanafu@tencent.com>
2024-12-06 09:52:13 +0800

    fix: 文件发送失败后会尝试其他worker成功重试优化 #330

--- src/backend/booster/bk_dist/controller/pkg/manager/local/mgr.go
+++ src/backend/booster/bk_dist/controller/pkg/manager/local/mgr.go
@@ -258,7 +258,7 @@ func (m *Mgr) ExecuteTask(
 			// 远程任务失败后，将文件大小和压缩大小都置为初始值，方便其他worker重试
 			for i, c := range remoteReq.Req.Commands {
 				for j, f := range c.Inputfiles {
-					if f.Compresstype < 0 && f.InitCompressedSize > 0 {
+					if f.CompressedSize < 0 && f.InitCompressedSize > 0 {
 						remoteReq.Req.Commands[i].Inputfiles[j].CompressedSize = remoteReq.Req.Commands[i].Inputfiles[j].InitCompressedSize
 					}
 					if f.FileSize < 0 && f.InitFileSize > 0 {
